46ae09bd22687695a2b3340d269623be60a91c17
Avery Magnotti <averymagnotti@outlook.com>
2017-07-23 11:00:34 -0400

    Add cch and upd aliases, add color to tests.sh

--- tests.sh
+++ tests.sh
@@ -8,18 +8,18 @@ fi
 
 source .aliases # expand aliases so we can test them
 
-ali=("-" ".." "..." "...." "~" "atom" "sh" "sudo") # alias names
-ali2=("cd -" "cd .." "cd ../.." "cd ../../.." "cd ~" "atom -a" "bash" "sudo ") # alias values
+ali=("-" ".." "..." "...." "~" "atom" "cch" "sh" "sudo" "upd") # alias names
+ali2=("cd -" "cd .." "cd ../.." "cd ../../.." "cd ~" "atom -a" "cache" "bash" "sudo " "update") # alias values
 
 echo ""
 
 for index in ${!ali[*]}; do # loop through both arrays ($ali, $ali2).
   out=`alias "${ali[$index]}" | grep -Po "'.*?'"` # strip out everything besides stuff in 'quotes'.
   if [ "$(alias ${ali[$index]})" != "alias ${ali[$index]}='${ali2[$index]}'" ]; then # if the alias name ($ali) does not equal to the alias value ($ali2), continue.
-    echo "ERR: '${ali[$index]}' == $out. Expected '${ali2[$index]}'." # print alias name and value, and return the expected value.
+    echo -e "\033[1;31mERR: '${ali[$index]}' == $out. Expected '${ali2[$index]}'.\033[0m" # print alias name and value, and return the expected value.
     export fail="true" # make a note that a test has failed.
   else
-    echo "OK: '${ali[$index]}' == $out" # mark aliases that produce correct values as OK.
+    echo -e "\033[1;32mOK:  '${ali[$index]}' == $out\033[0m" # mark aliases that produce correct values as OK.
   fi
 done
 
